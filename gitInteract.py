00c52ab318d06acfc6f8fe9b42b27a1964d252d0
theholycatfish <theholycatfish@gmail.com>
2021-11-17 08:22:07 -0600

    added some pygithub functions

--- gitInteract.py
+++ gitInteract.py
@@ -1,6 +1,6 @@
 import os, json
 
-from github import Github #pygithub https://github.com/PyGithub/PyGithubpip
+from github import Github #pygithub https://github.com/PyGithub/PyGithub
 import git # gitpython https://gitpython.readthedocs.io/
 import requests
 
@@ -28,6 +28,37 @@ def main():
         repos_id_lsit.append(repo.id)
     print(repos_id_lsit)
 
+
+def create_issue(g: str, repo_name: str, title: str, body: str):
+    """creates new issues in the specified repo
+    Args:
+        g: the github auth info to create the issue
+        repo_name: full repo name ex: ORG/Seeker_scanner
+        title: title of issue
+        body: body text of issue
+    """
+
+    repo = g.get_repo(repo_name)
+    repo.create_issue(title=title, body=body)
+
+def review_git_issues(repo_name: str):
+    """pulls up the existing issues in a repo
+    Args:
+        repo_name: full repo name ex: ORG/Seeker_scanner
+    """
+    issues_array = []
+    repo = g.get_repo(repo_name)
+    open_issues = repo.get_issues(state="open")
+    for oi in open_issues:
+        issues_array.append(oi.title)
+
+def git_clone(cloneurl: str):
+    """this clones a github repo
+    Args:
+        cloneurl: the git clone url for the repo
+    """
+
+    repo= git.Repo.clone_from(cloneurl, ".", depth=1) #depth is shallow
 
 
 def function_example(param1: int, param2: str) -> bool:
